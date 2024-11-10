70b815a89d6297119a67af695ee9a033591ded0d
AvazxonAD <asqarovavazxon77@gmail.com>
2024-11-10 12:06:44 +0500

    buglar togirlandi

--- src/rasxod.fio/rasxod.fio.service.js
+++ src/rasxod.fio/rasxod.fio.service.js
@@ -43,7 +43,7 @@ const paymentRequestService = async (account_number, batalon_id, from, to) => {
                 JOIN worker AS w ON w_t.worker_id = w.id
                 JOIN contract AS c ON c.id = t.contract_id
                 JOIN organization AS o ON o.id = c.organization_id
-                WHERE c.account_number_id = $1  AND c.doc_date BETWEEN $2 AND $3 AND t.batalon_id = $4
+                WHERE c.account_number_id = $1  AND c.doc_date BETWEEN $2 AND $3 AND t.batalon_id = $4 AND c.isdeleted = false
                     AND  0 = (SELECT (c.result_summa - COALESCE(SUM(summa), 0))::FLOAT FROM prixod WHERE isdeleted = false AND contract_id = c.id)
                     AND  NOT EXISTS (SELECT * FROM rasxod_fio WHERE isdeleted = false AND worker_task_id = w_t.id)
             )
@@ -55,7 +55,7 @@ const paymentRequestService = async (account_number, batalon_id, from, to) => {
                     JOIN task AS t ON t.id = w_t.task_id
                     JOIN contract AS c ON c.id = t.contract_id
                     JOIN organization AS o ON o.id = c.organization_id
-                    WHERE c.account_number_id = $1 AND c.doc_date BETWEEN $2 AND $3 AND t.batalon_id = $4 
+                    WHERE c.account_number_id = $1 AND c.doc_date BETWEEN $2 AND $3 AND t.batalon_id = $4 AND c.isdeleted = false
                         AND 0 = (SELECT (c.result_summa - COALESCE(SUM(summa), 0))::FLOAT FROM prixod WHERE isdeleted = FALSE AND contract_id = c.id) 
                         AND  NOT EXISTS (SELECT * FROM rasxod_fio WHERE isdeleted = false AND task_id = t.id)
                 ) AS itogo
