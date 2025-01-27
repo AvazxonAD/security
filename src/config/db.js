b1625914edf6b22021fa4828b36c851efb492fa8
AvazxonAD <asqarovavazxon77@gmail.com>
2025-01-28 00:51:44 +0500

    jurnal

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   host: process.env.DB_HOST, 
   user: process.env.DB_USER,
   port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD
 });
 
