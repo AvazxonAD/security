fe163abc8d48ab616c4b22045479ef8e24c6038e
AvazxonAD <asqarovavazxon77@gmail.com>
2025-04-11 10:13:13 +0500

    jurnal

--- src/contract/db.js
+++ src/contract/db.js
@@ -501,7 +501,7 @@ exports.getcontractService = async (
                     ${batalion_filter}
                     AND d.doc_date BETWEEN $4 AND $5 
                     AND d.account_number_id = $6
-                ORDER BY c.created_at DESC 
+                ORDER BY d.created_at DESC 
                 OFFSET $2 LIMIT $3
             )
             SELECT 
